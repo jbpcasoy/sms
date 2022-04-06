a4e31b01109ead29a8242934f981396ee6e86535
John Acaso <johnbryanpitcasoy@gmail.com>
2022-04-06 14:12:02 +0800

    added seeders

---
+++ database/factories/StudentFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Student>
+ */
+class StudentFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            "student_lrn" => Str::random(12),
+            "first_name" => $this->faker->firstName(),
+            "middle_name" => $this->faker->lastName(),
+            "last_name" => $this->faker->lastName(),
+            "age" => random_int(18, 25),
+            "year_level" => $this->faker->randomElement([
+                "1ST YEAR",
+                "2ND YEAR",
+                "3RD YEAR",
+                "4TH YEAR",
+            ]),
+            "section" => $this->faker->word(),
+        ];
+    }
+}
